50c5787f94a090bbdbb78063d297d043e007234a
belutiti <belen.tiaye@gmail.com>
2024-02-24 19:11:22 -0300

    Registrar producto/Mensaje de error

--- src/Components/registerProduct/RegisterProducts.jsx
+++ src/Components/registerProduct/RegisterProducts.jsx
@@ -1,88 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
 import RpStyles from "./RegisterProducts.module.css";
-import { useState } from "react";
+import axios from "axios";
 
 const RegisterProducts = () => {
-    // Estados para manejar los valores del formulario
-    const [nameProduct, setNameProduct] = useState ("");     //Manjea el valor del nombre
-    const [description, setDescription] = useState ("");     //Manjea el valor de la descripcion
-    const [price, setPrice] = useState ("");                 //Manjea el valor del precio
-    const [quotaOfPeople, setQuotaOfPeople] = useState (""); //Manjea el valor de la cantidad de personar que pueden enotarse en este viaje
-    const [imagen, setImagen] = useState(null)               //Manjea las imagenes del producto
+  const [nameProduct, setNameProduct] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [quotaOfPeople, setQuotaOfPeople] = useState("");
+  const [imagen, setImagen] = useState(null);
+  const [error, setError] = useState(null);
 
-    // Función para manejar el envío del formulario
-    const handleSubmit = (event) => {
-        event.preventDefault();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
+    try {
+      const response = await axios.post('http://localhost:3001/api/products', { name: nameProduct });
+
+      setError(null);
+
+     console.log(data)
+
+    } catch (error) {
+      setError(error.response.data.error);
     }
+  };
 
-    const handleImagenChange = (event) => {
-        const file = event.target.files[0]; // Obtenemos el primer archivo seleccionado
-        setImagen(file);
-      };
+  const handleImagenChange = (event) => {
+    const file = event.target.files[0];
+    setImagen(file);
+  };
 
-    return (
-        <div className={RpStyles.formContainer}>
+  return (
+    <div className={RpStyles.formContainer}>
+      <h2 className={RpStyles.title}> Registro de productos </h2>
 
-          <h2 className={RpStyles.title} > Registro de productos </h2>
+      <form onSubmit={handleSubmit}>
+        {error && (
+          <ErrorComponent
+            title="Error al registrar producto"
+            message={error}
+          />
+        )}
 
-        <form onSubmit={handleSubmit}>
-      <label>
-        <input
-          type="text"
-          value={nameProduct}
-          placeholder="Nombre"
-          onChange={(e) => setNameProduct(e.target.value)}
-          required
-        />
-      </label>
-      
-      <label>
-        <textarea
-          value={description}
-          placeholder="Descripcion"
-          onChange={(e) => setDescription(e.target.value)}
-          required
-        />
-      </label>
-      
-      <label>
-        <input
-          type="number"
-          value={price}
-          placeholder="Precio"
-          onChange={(e) => setPrice(e.target.value)}
-          required
-        />
-      </label>
-    
-      <label>
-        <input
-          type="number"
-          value={quotaOfPeople}
-          placeholder="Maximo de personas"
-          onChange={(e) => setQuotaOfPeople(e.target.value)}
-          required
-        />
-      </label>
-      
-      <label>
-        <input
-          type="file"
-          accept="image/*" // Acepta solo archivos de imagen
-          onChange={handleImagenChange}
-          required
-        />
-      </label>
-      
-      
-      
-      
-      <button type="submit">Agregar Producto</button>
-    </form>
-        </div>
-        
-    )
-}
+        <label>
+          <input
+            type="text"
+            value={nameProduct}
+            placeholder="Nombre"
+            onChange={(e) => setNameProduct(e.target.value)}
+            required
+          />
+        </label>
+
+        <label>
+          <textarea
+            value={description}
+            placeholder="Descripcion"
+            onChange={(e) => setDescription(e.target.value)}
+            required
+          />
+        </label>
+
+        <label>
+          <input
+            type="number"
+            value={price}
+            placeholder="Precio"
+            onChange={(e) => setPrice(e.target.value)}
+            required
+          />
+        </label>
+
+        <label>
+          <input
+            type="number"
+            value={quotaOfPeople}
+            placeholder="Maximo de personas"
+            onChange={(e) => setQuotaOfPeople(e.target.value)}
+            required
+          />
+        </label>
+
+        <label>
+          <input
+            type="file"
+            accept="image/*"
+            onChange={handleImagenChange}
+            required
+          />
+        </label>
+
+        <button type="submit">Agregar Producto</button>
+      </form>
+    </div>
+  );
+};
 
 export default RegisterProducts;
