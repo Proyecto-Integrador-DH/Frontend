7bf6c68f5f6c6f201101ea9ff6bd432c9b45df8a
belutiti <belen.tiaye@gmail.com>
2024-02-24 18:21:34 -0300

    mensaje error

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-import './App.css'
-import { Route, Routes } from 'react-router'
-import Products from './Routes/Products.jsx'
+import "./App.css";
+import { Route, Routes } from "react-router";
+import Products from "./Routes/Products.jsx";
 import HomePage from "./Pages/home/Home.jsx";
 
-
-function App  ()  {
+function App() {
 
   return (
-    <>
-      
+      <>
         <Routes>
-          <Route path='/Products' element={<Products/>}/>
-          <Route path='/' element={ <HomePage/>}/>
-          <Route path='/error' element={<ErrorComponent/>}/>
+          <Route path="/Products" element={<Products />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path='/registrarProducto' element={<RegisterProducts/>}/>
         </Routes>
-        
-    </>
-  )
+      </>
+  );
 }
 
-export default App
+export default App;
