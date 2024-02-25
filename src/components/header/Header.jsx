ddfb55e6748dd98ca08d6e23601d2d85dc041946
belutiti <belen.tiaye@gmail.com>
2024-02-25 20:02:44 -0300

    last

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,33 +1,29 @@
-import React from 'react';
-import HeaderStyle from './header.module.css';
-import Button from "../button/Button.jsx"
-import Logo from '../../assets/Logo03.png';
-
-
+import React from "react";
+import HeaderStyle from "./header.module.css";
+import Button from "../button/Button.jsx";
+import Logo from "../../assets/Logo03.png";
 
 const Header = () => {
-
   return (
-   <> 
-    <header>
-      <div className={HeaderStyle.logo}>
-        <div>
-        <img src={Logo} alt="" />
+    <>
+      <header>
+        <div className={HeaderStyle.logo}>
+          <div>
+            <img src={Logo} alt="" />
+          </div>
+          <div className={HeaderStyle.textContainer}>
+            <h2>Solo Aventuras</h2>
+            <p>compañia sin igual!</p>
+          </div>
         </div>
-        <div className={HeaderStyle.textContainer}>
-        <h2>Solo Aventuras</h2>
-        <p>compañia sin igual!</p>
-        </div>
-
-      </div>
 
         <div>
           <Button className={HeaderStyle.login}>Iniciar sesión</Button>
           <Button className={HeaderStyle.signup}>Crear Cuenta</Button>
         </div>
-    </header>
+      </header>
     </>
-    )
-}
+  );
+};
 
 export default Header;
