1b82aefc836d7bc984931b813d6cac6d3878784f
Liszinr <lisrueda3@outlook.com>
2024-02-27 11:33:52 -0500

    Diseño del encabezado completo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import HeaderStyle from './header.module.css';
 import Button from "../button/Button.jsx"
 import Logo from '../../assets/Logo.png';
@@ -6,8 +6,22 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
 
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollPosition(window.pageYOffset);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-   <div className={HeaderStyle.bloque}> 
+   <div id="navbar" className={`${HeaderStyle.bloque} ${scrollPosition > 0 ? 'bg-white' :  'bg-transparent'}`}> 
         <Link to='/'>
         <img src= {Logo} alt="logo"/>
         </Link>
@@ -15,6 +29,9 @@ const Header = () => {
           <Button className={HeaderStyle.login}>Iniciar sesión</Button>
           <Button className={HeaderStyle.signup}>Crear Cuenta</Button>
         </div>
+
+
+        
     </div>
   );
 };
