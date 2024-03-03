235aca3cbc5512dcade35cad21515208cdd41680
Huggodev <hugojavier02@gmail.com>
2024-03-03 10:47:02 -0300

    resolviendo caseSensitive

--- src/Components/footer/fontawesome.jsx
+++
@@ -1,4 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faFacebook, faTwitterSquare, faInstagramSquare } from '@fortawesome/free-brands-svg-icons';
-
-library.add(faFacebook, faTwitterSquare, faInstagramSquare);
---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ImagenBanner from "../../assets/Image02.png"
+import Explorer from "../../assets/Frame 6.png"
+import BannerStyles from "./Banner.module.css";
+import Button from "../button/Button.jsx"
+
+const Banner = () => {
+
+    return (
+        <div className={BannerStyles.bannerContainer}>
+            <div className={BannerStyles.bannerDivOne}>
+                <img src= {Explorer} alt="banner"/>
+                <h2>Desde</h2> 
+                <h2>Colombia </h2>
+                <h3>hasta Argentina </h3>
+                <p>Recuerda que nunca es tarde para cumplir tus sueños <br></br>aventureros, ¡así que planea tu próxima travesía con nosotros en cualquier momento!</p>
+            </div>
+            <div className={BannerStyles.bannerDivTwo}>
+                <img src= {ImagenBanner} alt="banner"/>
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
