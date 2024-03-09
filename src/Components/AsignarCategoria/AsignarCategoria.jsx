235dfc77f9cc7c9bcec12823359e610783830d51
Adriana Paredes Salinas <adrianaparedessalinas@gmail.com>
2024-03-09 15:26:05 -0500

    Mejorando la tabla de categorías

--- src/Components/AsignarCategoria/AsignarCategoria.jsx
+++ src/Components/AsignarCategoria/AsignarCategoria.jsx
@@ -1,44 +1,43 @@
-import React, { useState, useEffect }from 'react'
-import { fetchCategorias } from '../../services/api';
+import React, { useState, useEffect } from "react";
+import { fetchCategorias } from "../../services/api";
 
 const AsignarCategoria = () => {
-    const [categorias, setCategoria] = useState([]);
+  const [categorias, setCategoria] = useState([]);
 
+  useEffect(() => {
+    fetchCategorias()
+      .then((data) => {
+        setCategoria(data);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error(errorHandling(error));
+      });
+  }, []);
 
-	useEffect(() => {
+  return (
+    <div className="overflow-hidden">
+      <h2 className="text-3xl font-bold mb-6">Lista de categorias</h2>
+      <div className="overflow-x-auto">
+        <table className="max-w-m whitespace-nowrap table-auto">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nombre de la experiencia</th>
+            </tr>
+          </thead>
+          <tbody>
+            {categorias.map((categoria) => (
+              <tr key={categoria.id}>
+                <td>{categoria.id}</td>
+                <td className="capitalize">{categoria.nombre}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
 
-		fetchCategorias()
-			.then(data => {
-				setCategoria(data)
-				console.log(data);
-			})
-			.catch(error => {
-				console.error(errorHandling(error));
-			});
-	}, []);
-
-
-	return (
-		<div>
-			<h2>Lista de categorias</h2>
-			<table>
-				<thead>
-					<tr>
-						<th>ID</th>
-						<th>Nombre de la experiencia</th>
-					</tr>
-				</thead>
-				<tbody>
-					{categorias.map(categoria => (
-						<tr key={categoria.id}>
-							<td>{categoria.id}</td>
-							<td>{categoria.nombre}</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
-		</div>
-	);
-}
-
-export default AsignarCategoria
+export default AsignarCategoria;
--- src/Components/ListarUsuarios/ListarUsuarios.jsx
+++ src/Components/ListarUsuarios/ListarUsuarios.jsx
@@ -1,8 +1,12 @@
-import React, { useEffect, useState } from 'react'
-import { errorHandling } from '../../services/errorHandling';
-import { fetchAsignarRol, fetchListarUsuarios, fetchQuitarRol } from '../../services/api';
-import style from './ListarUsuarios.module.css';
-import ErrorComponent from '../error/ErrorAlert';
+import React, { useEffect, useState } from "react";
+import { errorHandling } from "../../services/errorHandling";
+import {
+  fetchAsignarRol,
+  fetchListarUsuarios,
+  fetchQuitarRol,
+} from "../../services/api";
+import style from "./ListarUsuarios.module.css";
+import ErrorComponent from "../error/ErrorAlert";
 
 const ListarUsuarios = () => {
   const [usuarios, setUsuarios] = useState([]);
@@ -12,11 +16,11 @@ const ListarUsuarios = () => {
 
   useEffect(() => {
     fetchListarUsuarios()
-      .then(data => {
+      .then((data) => {
         setUsuarios(data);
         console.log(data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(errorHandling(error));
       });
   }, []);
@@ -25,27 +29,31 @@ const ListarUsuarios = () => {
     try {
       const data = await fetchListarUsuarios();
       setUsuarios(data);
-      console.log('Lista de usuarios actualizada');
+      console.log("Lista de usuarios actualizada");
     } catch (error) {
-      console.error('Error al actualizar la lista de usuarios:', error.message);
-      console.error('Error al actualizar la lista de usuarios. Por favor, inténtalo de nuevo.');
+      console.error("Error al actualizar la lista de usuarios:", error.message);
+      console.error(
+        "Error al actualizar la lista de usuarios. Por favor, inténtalo de nuevo."
+      );
     }
   };
 
   const handleAsignarRol = async (usuarioId, rolId) => {
     const id = Number(usuarioId);
     try {
-      const response = await fetchAsignarRol({ id, roles: [{ id: rolId }] });
-      console.log('Rol asignado correctamente');
+      await fetchAsignarRol({ id, roles: [{ id: rolId }] });
+      setModalErrorVisible(true);
+      setTitleError("Rol actualizado");
+      setError("El rol se asignó correctamente");
       await refrescarUsuarios(); // Refrescar la lista de usuarios después de asignar el rol
     } catch (error) {
-      if(error = 400){
+      if ((error = 400)) {
         setModalErrorVisible(true);
         setTitleError("Error");
         setError("El usuario ya tiene el rol seleccionado");
       }
-      console.error('Error al asignar el rol:', error.message);
-      console.error('Error al asignar el rol. Por favor, inténtalo de nuevo.');
+      console.error("Error al asignar el rol:", error.message);
+      console.error("Error al asignar el rol. Por favor, inténtalo de nuevo.");
     }
   };
 
@@ -53,16 +61,16 @@ const ListarUsuarios = () => {
     const id = Number(usuarioId);
     try {
       await fetchQuitarRol({ id, roles: [{ id: rolId }] });
-      console.log('Rol quitado correctamente');
+      console.log("Rol quitado correctamente");
       await refrescarUsuarios(); // Refrescar la lista de usuarios después de quitar el rol
     } catch (error) {
-      if(error = 400){
+      if ((error = 400)) {
         setModalErrorVisible(true);
         setTitleError("Error");
         setError("El usuario no es administrador");
       }
-      console.error('Error al quitar el rol:', error.message);
-      console.error('Error al quitar el rol. Por favor, inténtalo de nuevo.');
+      console.error("Error al quitar el rol:", error.message);
+      console.error("Error al quitar el rol. Por favor, inténtalo de nuevo.");
     }
   };
 
@@ -95,20 +103,31 @@ const ListarUsuarios = () => {
           </tr>
         </thead>
         <tbody>
-          {usuarios.map(usuario => (
+          {usuarios.map((usuario) => (
             <tr key={usuario.id}>
               <td>{usuario.id}</td>
               <td>{usuario.nombre}</td>
               <td>{usuario.apellido}</td>
               <td>{usuario.email}</td>
               <td>
-                {usuario.roles && usuario.roles.map(rol => (
-                  <span key={rol.id}>{rol.nombre}, </span>
-                ))}
+                {usuario.roles &&
+                  usuario.roles.map((rol) => (
+                    <span key={rol.id}>{rol.nombre}, </span>
+                  ))}
               </td>
               <td>
-                <button className={style.button} onClick={() => handleAsignarRol(usuario.id, 1)}>Asignar</button>
-                <button className={style.button} onClick={() => handleQuitarRol(usuario.id, 2)}>Quitar</button>
+                <button
+                  className={style.button}
+                  onClick={() => handleAsignarRol(usuario.id, 1)}
+                >
+                  Asignar
+                </button>
+                <button
+                  className={style.button}
+                  onClick={() => handleQuitarRol(usuario.id, 2)}
+                >
+                  Quitar
+                </button>
               </td>
             </tr>
           ))}
