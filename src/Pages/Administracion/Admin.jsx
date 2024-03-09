5e90319a05ae6362865517ebb0a72f5b90862569
Adriana Paredes Salinas <adrianaparedessalinas@gmail.com>
2024-03-09 13:46:13 -0500

    Restricción del panel de administración en mobile

--- src/Pages/Administracion/Admin.jsx
+++ src/Pages/Administracion/Admin.jsx
@@ -4,45 +4,36 @@ import Button from "../../Components/button/Button";
 import AdminStyle from './Administrador.module.css';
 import { Link } from 'react-router-dom';
 
-
 const Adminstrador = () => {
-
-
   return (
     <div>
-
-      <h2 className={AdminStyle.h2}> Panel de administración</h2>
-
-      <div className={AdminStyle.contarjetas}>
-
-        <div className={AdminStyle.tarjetas}>
-          <h2>Agregar una experiencia</h2>
-          <Link to='/registrarProducto' > <Button className={AdminStyle.boton}>Clik aqui </Button></Link>
+      <h2 className={AdminStyle.h2}>Panel de administración</h2>
+      <h3 className={AdminStyle.h3}>No puedes acceder al panel con un dispositivo móvil</h3>
+      <div className={AdminStyle.contenedor}>
+        <div className={AdminStyle.tarjetasContainer}>
+          <div className={AdminStyle.tarjetas}>
+            <h2>Agregar una experiencia</h2>
+            <Link to='/registrarProducto'><Button className={AdminStyle.boton}>Clic aquí</Button></Link>
+          </div>
+          <div className={AdminStyle.tarjetas}>
+            <h2>Lista de experiencias</h2>
+            <Link to='/listarProductos'><Button className={AdminStyle.boton}>Clic aquí</Button></Link>
+          </div>
         </div>
-
-
-        <div className={AdminStyle.tarjetas}>
-          <h2>Lista de experiencias</h2>
-          <Link to='/listarProductos' ><Button className={AdminStyle.boton}>Click aqui</Button></Link>
+        <div className={AdminStyle.tarjetasContainer}>
+          <div className={AdminStyle.tarjetas}>
+            <h2>Lista de Categorías</h2>
+            <Link to='/asignarCategoria'><Button className={AdminStyle.boton}>Clic aquí</Button></Link>
+          </div>
+          <div className={AdminStyle.tarjetas}>
+            <h2>Listar Usuarios</h2>
+            <Link to='/listarUsuarios'><Button className={AdminStyle.boton}>Clic aquí</Button></Link>
+          </div>
         </div>
-
-        <div className={AdminStyle.tarjetas}>
-          <h2>Lista de Categorías</h2>
-          <Link to='/asignarCategoria'><Button className={AdminStyle.boton}>Click aqui</Button></Link>
-        </div>
-
-        <div className={AdminStyle.tarjetas}>
-          <h2>Listar Usuarios</h2>
-          <Link to='/listarUsuarios'><Button className={AdminStyle.boton}>Click aqui</Button></Link>
-        </div>
-
       </div>
-
-
       <Footer />
     </div>
-
-  )
+  );
 }
 
-export default Adminstrador
+export default Adminstrador;
