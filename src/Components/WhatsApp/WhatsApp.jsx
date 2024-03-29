2e30bb311a151cd363fd6e838834ad29de903c5a
Adriana Paredes Salinas <adrianaparedessalinas@gmail.com>
2024-03-29 11:11:39 -0500

    Confuguración de WhatsApp

---
+++ src/Components/WhatsApp/WhatsApp.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { FloatingWhatsApp } from "react-floating-whatsapp";
+import Solologo from "../../assets/Solologo.png";
+import style from "./WhatsApp.module.css";
+
+const WhatsAppFloatingButton = () => {
+
+  return (
+    <FloatingWhatsApp
+      avatar={Solologo}
+      accountName="Solo Aventuras"
+      phoneNumber="573186219938"
+      message="Hola, ¿cómo puedo ayudarte?"
+      statusMessage="Estamos en línea"
+      chatMessage="Hola! Somos Solo Aventuras, queremos que hagas match con tu próxima experiencia. ¿En qué podemos ayudarte?"
+      placeholder="Escribe tu mensaje aquí"
+      chatboxStyle={style}
+      buttonClassName={style.button}
+    />
+  );
+};
+
+export default WhatsAppFloatingButton;
